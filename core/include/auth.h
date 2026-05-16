52a109c64a7e169f9b647d3427607b1e9300df81
Kr1m12 <galashov16@gmail.com>
2026-05-16 19:43:35 +0000

    feat: add skeleton auth and crypto headers for secure_messenger

--- core/include/auth.h
+++ core/include/auth.h
@@ -1,1 +1,19 @@
-// auth.h - Header declarations will follow
+#ifndef SECURE_MESSENGER_AUTH_H
+#define SECURE_MESSENGER_AUTH_H
+
+#include <string>
+
+struct UserRecord {
+    std::string login;
+    std::string hash;
+    std::string salt;
+};
+
+class AuthManager {
+public:
+    bool register_user(const std::string& login, const std::string& password);
+    bool verify_user(const std::string& login, const std::string& password,
+                     const std::string& hash, const std::string& salt);
+};
+
+#endif // SECURE_MESSENGER_AUTH_H
--- core/include/crypto.h
+++ core/include/crypto.h
@@ -1,1 +1,13 @@
-// crypto.h - Header declarations will follow
+#ifndef SECURE_MESSENGER_CRYPTO_H
+#define SECURE_MESSENGER_CRYPTO_H
+
+#include <string>
+
+class CryptoEngine {
+public:
+    std::string generate_salt();
+    std::string hash_password(const std::string& password, const std::string& salt);
+    bool secure_compare(const std::string& a, const std::string& b);
+};
+
+#endif // SECURE_MESSENGER_CRYPTO_H
